ee5ea26d4da88287034048a486360952bd52fc51
Dmitri Peredera <dmitri.peredera@gmail.com>
2015-09-25 15:09:08 +0300

    "cannot change value of locked binding for data" error fixed with renaming.

--- run_analysis.R
+++ run_analysis.R
@@ -80,22 +80,24 @@ AssignNamesToDt <- function(){
   colnames(dt.train.x) <<- dt.features[,2]
   colnames(dt.test.x) <<- dt.features[,2];
   
-  # TODO: assign Y names.
+  # TODo: Assign Y names. activity label.
+  #colnames(dt.train.y) <<- "activityType";
+  #colnames(dt.test.y) <<- "activityType";
 }
 
 # Merge all sets to a single table.
 MergeTablesToOne <- function(){
   # 1.Merges the training and the test sets to create one data set.
-  dt.merged.subject <- rbind(dt.subject.test, dt.subject.train)
-  dt.merged.x <- rbind(dt.train.x, dt.test.x)
-  dt.merged.y <- rbind(dt.train.y, dt.test.y) 
+  dt.merged.subject <<- rbind(dt.subject.test, dt.subject.train)
+  dt.merged.x <<- rbind(dt.train.x, dt.test.x)
+  dt.merged.y <<- rbind(dt.train.y, dt.test.y) 
   
   # Merge all datasets by rows. 
-  data <- cbind(dt.merged.subject, dt.merged.y, dt.merged.x) 
+  dataAll <<- cbind(dt.merged.subject, dt.merged.y, dt.merged.x) 
   
   # For private use.
-  data.size <- object.size(data)                 
-  data.size <- format(data.size, quote = FALSE, units = "MB") # Appr. 42Mb.
+  data.size <<- object.size(dataAll)                 
+  data.size <<- format(data.size, quote = FALSE, units = "MB") # Appr. 42Mb.
   message("Merged table size = ", data.size)
 }
 
@@ -124,5 +126,5 @@ rm(dt.merged.subject, dt.merged.x, dt.merged.y)
 
 # 2.Extracts only the measurements on the mean and standard deviation for 
 # each measurement. Id's are also removed.
-dataFiltered  <- data[,grepl("mean|std", names(data))] # Only 79 rows now.
+dataFiltered  <- dataAll[,grepl("mean|std", names(dataAll))] # Only 79 rows now.
 
